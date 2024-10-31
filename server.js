9024027517b0717889c3e1f4948e7d824de4b1b7
ogg1996 <rjsrnr43@naver.com>
2024-11-01 02:31:40 +0900

    서버 수정, image 파일 추가

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const jsonServer = require("json-server");
+const express = require("express");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+
+server.use("/images", jsonServer.static("images"));
 
 server.listen(3000, () => {
   console.log("JSON Server is running port:3000");
